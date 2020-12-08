aa4c4a783ec391d83a69edb186a5ddd5f74e3d88
Lowe Stålnacke <lowe.stalnacke@konsult.atg.se>
2020-12-08 15:18:17 +0100

    Folder structure kind of

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const dayOne = require('./1');
-const dayTwo = require('./2');
-const dayThree = require('./3');
-const dayFour = require('./4');
-const dayFive = require('./5');
-const daySix = require('./6');
-const daySeven = require('./7');
-const dayEight = require('./8');
+const dayOne = require('./src/1');
+const dayTwo = require('./src/2');
+const dayThree = require('./src/3');
+const dayFour = require('./src/4');
+const dayFive = require('./src/5');
+const daySix = require('./src/6');
+const daySeven = require('./src/7');
+const dayEight = require('./src/8');
 
 // TODO: Something useful
 console.log([
